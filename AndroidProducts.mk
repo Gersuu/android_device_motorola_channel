3844d43495115f21231695175f3317549fbaeeae
Gersuu <62456969+Gersuu@users.noreply.github.com>
2020-10-04 20:24:54 -0300

    channel:DerpFest bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_channel.mk
+    $(LOCAL_DIR)/derp_channel.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_channel-eng \
-    lineage_channel-user \
-    lineage_channel-userdebug
+    derp_channel-eng \
+    derp_channel-user \
+    derp_channel-userdebug
